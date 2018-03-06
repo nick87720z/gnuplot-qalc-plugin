7f33186c652c587edb97b243f4a654d52d690a94
Nikita Zlobin <nick87720z@gmail.com>
2018-03-07 01:55:34 +0500

    Final improvement for formula expression optimization

--- gnuplot-qalc-plugin.cc
+++ gnuplot-qalc-plugin.cc
@@ -167,12 +167,9 @@ extern "C" {
             UserFunction ufunc ("", "", arg[1].v.string_val);
             MathStructure args_v (symstruct[0], NULL);
 
-            //~ mstruct = MathStructure();
             ufunc.calculate(*mstruct, args_v, eopt);
             mstruct->replace(v_xstruct, *(symstruct[0]));
-            mstruct->factorize(eopt);
             mstruct->eval(eopt);
-            mstruct->simplify(eopt);
 
             ufunc_v.push_back (mstruct);
             ufunc_names.push_back (strdup(arg[0].v.string_val));
